d646a246180d93c99d418da03514f5514dc097b5
Caroline Loppi <loppi.clg@gmail.com>
2022-06-19 10:25:07 -0300

    Cria tela inicial de login

--- MeuPrimeiroAppIOS/ViewController.swift
+++ MeuPrimeiroAppIOS/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var txtLogin: UITextField!
+    
+    @IBOutlet weak var txtPassword: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    @IBAction func openAction(_ sender: Any) {
+        
+        if(txtLogin.text == "carol" && txtPassword.text == "1234"){
+          performSegue(withIdentifier: "initialScreen", sender: self)
+            
+        }
+        else{
+            let alert = UIAlertController(title: "Alerta", message: "Erro no Login/Senha", preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        
+        }
+    }
+    
 
 }
 
